29af5dd49b40d5d4daa48bed5ef3adabd7f3044b
DiegodaGuia <diegodguia@gmail.com>
2024-09-14 17:33:23 +0000

    commit 7 - função potenciação

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,4 +1,6 @@
-// Função de multiplicação
-function multiply(a, b) { return a * b; }
+// Função de potenciação
+function power(base, exponent) {
+  return Math.pow(base, exponent);
+}
 // Exportar as funções
-module.exports = { add, subtract, multiply };
+module.exports = { add, subtract, multiply, divide, power };
